33a249a426437f03a9b56663bd7112cabfc4007c
Your Name <your.email@example.com>
2025-01-15 16:01:18 +0800

    testing

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 // Sample data
 const data = [
     { id: 1, name: "Item 1", description: "PLEASE BAGI JADI" },
-    { id: 2, name: "Item 2", description: "HELLO GUYSS" },
+    { id: 2, name: "Item 2,", description: "HELLO and hi GUYSS" },
     { id: 3, name: "Item 3", description: "PIJA TESTINGGG " },
 ];
 
