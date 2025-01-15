045fb0fa4a57f3c196ffbf5ca038cd590d86d646
Your Name <your.email@example.com>
2025-01-15 15:48:09 +0800

    dah tukaq namaa pija

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ app.use(cors());
 
 // Sample data
 const data = [
-    { id: 1, name: "Item 1", description: "This is the first item" },
-    { id: 2, name: "Item 2", description: "This is the second item" },
+    { id: 1, name: "Item 1", description: "PLEASE BAGI JADI" },
+    { id: 2, name: "Item 2", description: "HELLO GUYSS" },
     { id: 3, name: "Item 3", description: "PIJA TESTINGGG " },
 ];
 
